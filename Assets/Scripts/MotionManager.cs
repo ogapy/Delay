dace37cb5d66353411b68e32ade2764a075c7f76
ogapy <kyon6690@gmail.com>
2020-10-25 02:02:11 +0900

    物体を手の動きに追従させる

---
+++ Assets/Scripts/MotionManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+namespace Forgevision.InputCapture
+{
+    public class MotionManager : MonoBehaviour
+    {
+        [SerializeField]
+        MotionRecorder _motionRecorder;
+        [SerializeField]
+        MotionPlayer _motionPlayer;
+        MotionClip _motionClip = new MotionClip();
+        public float _delayTimeSec = 1f;
+        void Start()
+        {
+        _motionRecorder.StartRecord(_motionClip);
+        _motionPlayer.MotionPlay(_motionClip, _delayTimeSec);
+        }
+    }
+}
